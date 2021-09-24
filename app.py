2bba2e1407fa2b8a2797a8d2d2b2a080cbb0f6a6
hosseinmote <motevas2.illinois.edu>
2021-09-24 00:59:51 -0500

    bug: None API keys

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ The focus is on your goals and health in an effortless and easiest way possible 
                 
         # use the Nutritionix API to extract food items and serving sizes
         nx_api = ns.creds()  
+        print("hello", nx_api)
         food_list = ns.analyze(recog_text, nx_api)
         food_macros = ns.extract_macros(food_list['foods'])
         
